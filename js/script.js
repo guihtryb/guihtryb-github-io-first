8e7cd0a9e7e3593156a1ac7f0d2a4580498bf68a
Guilherme Viana <guilhermehviana01@gmail.com>
2022-08-03 18:27:15 -0300

    update: atualizando textos e estilo de introdução

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-import IntroductionHeaderBreaksControl from "./modules/IntroductionBreaks.js";
 import Modal from "./modules/modal.js";
 import SmoothScrolling from "./modules/smoothScrolling.js";
-
-const introductionBreaksControl = new IntroductionHeaderBreaksControl('.hi-roles h1');
 
 const smoothScrolling = new SmoothScrolling(
   '.page-menu a[href^="#"]',
@@ -12,7 +9,6 @@ const smoothScrolling = new SmoothScrolling(
 const modal = new Modal('[data-modal="show"]', '[data-modal="hide"]', '[data-modal="container"]', 'active');
 
 window.onload = () => {
-  introductionBreaksControl.init();
   smoothScrolling.init();
   modal.init();
 };
