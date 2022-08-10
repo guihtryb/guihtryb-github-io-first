33b6da3291a93f8784137a4554b504b3427162dc
Guilherme Viana <guilhermehviana01@gmail.com>
2022-08-10 20:05:20 -0300

    build: projects object

---
+++ js/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    name: "getIt",
+    images: {
+      image1: "./img/projects/get-it/user-view.png",
+      image2: "./img/projects/get-it/login-page.png",
+      image3: "./img/projects/get-it/admin-view.png",
+      image4: "./img/projects/get-it/admin-editing.png",
+      },
+    hasSite: false,
+    siteLink: '',
+    gitHubLink: "https://github.com/guihtryb/get-it-controle-de-pontos"
+  },
+  {
+    name: "bikcraft",
+    images: {
+        image1: "./img/projects/bikcraft/bikcraft1.png",
+        image2: "./img/projects/bikcraft/bikcraft2.png",
+        image3: "./img/projects/bikcraft/bikcraft3.png",
+        image4: "./img/projects/bikcraft/bikcraft4.png",
+      },
+    hasSite: true,
+    siteLink: 'https://guihtryb.github.io/bikcraft',
+    gitHubLink: "https://github.com/guihtryb/bikcraft"
+  }
+];
+
+export default projects;
